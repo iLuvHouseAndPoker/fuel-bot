9ef89761535788ae33b9b6258930c05f48e29092
iLuvHouseAndPoker <goldenamir@mail.ru>
2025-08-14 19:02:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telegram.ext import Application, CommandHandler, MessageHandler, ContextTyp
 
 # === Переменные окружения ===
 TOKEN = os.environ["8212886572:AAEFoF7TJq8265YWx8JiQ8WAONSizMwUKs0"]           # токен бота от BotFather
-APP_URL = os.environ["APP_URL"]           # публичный URL сервиса (напр. https://fuel-bot.onrender.com)
+APP_URL = os.environ["https://fuel-bot-jsek.onrender.com"]           # публичный URL сервиса (напр. https://fuel-bot.onrender.com)
 
 # === FastAPI-приложение и Telegram Application ===
 app = FastAPI()
